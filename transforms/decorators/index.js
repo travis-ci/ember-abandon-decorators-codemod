69e9409f29a783ef7a677dbaa935321d8cc2f812
Buck Doyle <b@chromatin.ca>
2018-11-28 16:15:41 -0600

    Add section comments

--- transforms/decorators/index.js
+++ transforms/decorators/index.js
@@ -4,6 +4,12 @@ module.exports = function transformer(file, api) {
   let j = getParser(api);
   let root = j(file.source);
 
+  /*
+   * import { service } from 'ember-decorators/service';
+   * becomes
+   * import { inject as service } from '@ember/service';
+  */
+
   let serviceImportStatements = root.find(j.ImportDeclaration, {
     source: { value: 'ember-decorators/service' }
   });
@@ -13,6 +19,12 @@ module.exports = function transformer(file, api) {
   
     importStatement.replace(j.importDeclaration(variable, j.literal('@ember/service')));
   });
+
+  /*
+   * @service scroller: null,
+   * becomes
+   * scroller: service(),
+   */
 
   let serviceInjections = root.find(j.ObjectProperty, {
     decorators: [{ type: 'Decorator', expression: { type: 'Identifier', name: 'service'} }]
@@ -24,6 +36,12 @@ module.exports = function transformer(file, api) {
     injection.replace(j.objectProperty(key, value));
   });
 
+  /*
+   * @service('broadcasts') broadcastsService: null,
+   * becomes
+   * broadcastsService: service('broadcasts'),
+   */
+
   let renamedServiceInjections = root.find(j.ObjectProperty, {
     decorators: [{type: 'Decorator', expression: { type: 'CallExpression', callee: { type: 'Identifier', name: 'service' }}}]
   });
@@ -33,6 +51,12 @@ module.exports = function transformer(file, api) {
     let value = j.callExpression(j.identifier('service'), [j.literal(injection.value.decorators[0].expression.arguments[0].value)]);
     injection.replace(j.objectProperty(key, value));
   });
+
+  /*
+   * import { controller } from 'ember-decorators/controller';
+   * becomes
+   * import { inject as controller } from '@ember/controller';
+   */
 
   let controllerImportStatements = root.find(j.ImportDeclaration, {
     source: { value: 'ember-decorators/controller' }
@@ -44,6 +68,12 @@ module.exports = function transformer(file, api) {
     importStatement.replace(j.importDeclaration(variable, j.literal('@ember/controller')));
   });
 
+  /*
+   * @controller('intersectionality') intersectionalityController: null,
+   * becomes
+   * intersectionalityController: controller('intersectionality'),
+   */
+
   let renamedControllerInjections = root.find(j.ObjectProperty, {
     decorators: [{type: 'Decorator', expression: { type: 'CallExpression', callee: { type: 'Identifier', name: 'controller' }}}]
   });
@@ -54,6 +84,12 @@ module.exports = function transformer(file, api) {
     injection.replace(j.objectProperty(key, value));
   });
 
+  /*
+   * import { alias, sort } from 'ember-decorators/object/computed';
+   * becomes
+   * import { alias, sort } from '@ember/object/computed';
+   */
+
   let macroImportStatements = root.find(j.ImportDeclaration, {
     source: { value: 'ember-decorators/object/computed' }
   });
@@ -63,6 +99,17 @@ module.exports = function transformer(file, api) {
   
     importStatement.replace(j.importDeclaration(variable, j.literal('@ember/object/computed')));
   });
+
+  /*
+   * @sort('oppressions', (a, b) => {
+   *   return Math.random();
+   * })
+   * oppressionOlympics: null,
+   * becomes
+   * oppressionOlympics: sort('oppressions', (a, b) => {
+   *   return Math.random();
+   * }),
+   */
 
   let selectedMacros = ['alias', 'and', 'empty', 'equal', 'filter', 'filterBy', 'gt', 'mapBy', 'not', 'notEmpty', 'oneWay', 'or', 'reads', 'sort'];
 
@@ -86,6 +133,12 @@ module.exports = function transformer(file, api) {
     macro.replace(j.objectProperty(key, value));
   });
 
+  /*
+   * import { attr } from 'ember-decorators/data';
+   * becomes
+   * import attr from 'ember-data/attr';
+   */
+
   let attrImportStatements = root.find(j.ImportDeclaration, {
     source: { value: 'ember-decorators/data' }
   });
@@ -95,6 +148,14 @@ module.exports = function transformer(file, api) {
   
     importStatement.replace(j.importDeclaration([j.importDefaultSpecifier(j.identifier(variable))], j.literal('ember-data/attr')));
   });
+
+  /*
+   * @attr untypedAttr: '',
+   * @attr('string') typedAttr: null,
+   * become
+   * untypedAttr: attr(),
+   * typedAttr: attr('string'),
+   */
 
   let attrs = root.find(j.ObjectProperty, (property) => {
     if (!property.decorators || !property.decorators[0]) {
@@ -118,6 +179,11 @@ module.exports = function transformer(file, api) {
     attr.replace(j.objectProperty(key, value));
   });
 
+  /*
+   * import { action } from 'ember-decorators/object';
+   * is removed
+   */
+
   root.find(j.ImportDeclaration, {
     source: { value: 'ember-decorators/object' }
   }).forEach(actionImport => actionImport.prune());
@@ -125,6 +191,19 @@ module.exports = function transformer(file, api) {
   let actions = root.find(j.ObjectMethod, {
     decorators: [{ type: 'Decorator', expression: { type: 'Identifier', name: 'action'} }]
   });
+
+  /*
+   * @action
+   * joinTheResistance(name, location) {
+   *   alert(`Thanks for joining the struggle, ${name} from ${location}!`);
+   * },
+   * becomes
+   * actions: {
+   *   joinTheResistance(name, location) {
+   *     alert(`Thanks for joining the struggle, ${name} from ${location}!`);
+   *   },
+   * }
+   */
 
   if (actions.length > 0) {
     let methods = [];
